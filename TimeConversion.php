c7741858b4c1ab16dfc54b9becb42de01aad288e
Mamun Ahmed <maamun7@gmail.com>
2020-06-02 13:05:43 +0600

    Time conversion solved !

--- TimeConversion.php
+++ TimeConversion.php
@@ -4,9 +4,22 @@
  * @title Time Conversion
  * @link https://www.hackerrank.com/challenges/birthday-cake-candles/problem
  */
+// sample
+// $s = '07:05:45PM';
 
 function timeConversion($s) {
+    $format = strtolower(substr($s, -2));
+    $wfs = substr($s, 0, -2); // cut string am/pm [wfs = withoutFormatString]
+    $sta = explode(':', $wfs); // string to array [sta = stringToArray]
+    $hour = $sta[0];// getting hour from zero index of array
 
+    if($hour == 12 && $format =='am') {
+        $sta[0] = '00';
+    } else if($hour < 12 && $format =='pm') {
+        $sta[0] = 12 + $hour;
+    }
+
+    return implode(':', $sta);
 }
 
 $fptr = fopen(getenv("OUTPUT_PATH"), "w");
